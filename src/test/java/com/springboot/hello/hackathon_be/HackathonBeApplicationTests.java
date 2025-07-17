2ca39db2112d54c281ee9099e07f009561e0ec94
HeewonJo <heewonjo.me@gmail.com>
2025-07-17 14:33:00 +0900

    :gear:Setting

---
+++ src/test/java/com/springboot/hello/hackathon_be/HackathonBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.hello.hackathon_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
